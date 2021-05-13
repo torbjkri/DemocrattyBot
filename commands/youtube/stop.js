fe29e5ec05bc417391272f104130f7d691955d6f
Torbjorn Kringeland <tkringel@cisco.com>
2021-05-13 10:39:18 +0200

    Started stop command

--- commands/youtube/stop.js
+++ commands/youtube/stop.js
@@ -1,3 +1,4 @@
+const { MessageFlags } = require('discord.js');
 const { Command } = require('discord.js-commando');
 
 module.exports = class StopCommand extends Command {
@@ -7,11 +8,19 @@ module.exports = class StopCommand extends Command {
             group: 'youtube',
             memberName: 'stop',
             description: 'Admin command: stops music player',
-            ownerOnly: true
+            ownerOnly: true,
+            this.clientPermissions: ['SPEAK', 'CONNECT'],
         });
     }
 
     run(message) {
-        return message.say(`${message.content.split(" ")[1]} is a pinis!`);
+        let voiceChannel = message.member.voice.channel;
+        if (!voiceChannel) {
+            return message.reply("you need to be in the voice channel to interact with the bot.")
+        }
+        if (message.guild.musicData.songDispatcher === null) {
+            return message.reply("there is no music to stop, it's in your head!");
+        }
+        const queue =  message.guild.musicData.queue;
     }
 };
